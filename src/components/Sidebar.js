37eb0fc38083405aa6f2b7fa66b4af63f1c1f161
sujeong126 <hsjhsj8010@naver.com>
2023-03-23 01:46:34 +0900

    Style: Sidebar size fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,9 @@
 import style from "../styles/Sidebar.module.scss";
 import logo from "../assets/logo.png";
-import home from "../assets/home.png";
-import book from "../assets/book.png";
-import movie from "../assets/movie.png";
-import heart from "../assets/heart.png";
+import home_icon from "../assets/icon-Home.svg";
+import magazine_icon from "../assets/icon-Magazine.svg";
+import cinema_icon from "../assets/icon-cinema.svg";
+import wishlist_icon from "../assets/icon-wishlist.svg";
 
 function Sidebar() {
   return (
@@ -17,25 +17,25 @@ function Sidebar() {
         <ul className={style.nav}>
           <li>
             <a href="{()=>false}">
-              <img src={home} alt="home_img"/>
+              <img src={home_icon} alt="home_icon"/>
               <span>홈</span>
             </a>
           </li>
           <li>
             <a href="{()=>false}">
-              <img src={book} alt="book_img"/>
+              <img src={magazine_icon} alt="magazine_icon"/>
               <span>매거진</span>
             </a>
           </li>
           <li>
             <a href="{()=>false}">
-              <img src={movie} alt="movie_img"/>
+              <img src={cinema_icon} alt="cinema_icon"/>
               <span>상영회</span>
             </a>
           </li>
           <li>
             <a href="{()=>false}">
-              <img src={heart} alt="heart_img"/>
+              <img src={wishlist_icon} alt="wishlist_icon"/>
               <span>위시리스트</span>
             </a>
           </li>
