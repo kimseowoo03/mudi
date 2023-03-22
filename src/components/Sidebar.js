75e7f9d07f91891ac6cdff4c7222e996f70b07e0
kimseowoo03 <kimseowoo03@gmail.com>
2023-03-23 03:25:05 +0900

    Refactor: 코드를 리액트 컴포넌트로 재구성함

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,35 @@
+import NavItem from "./NavItem";
+
 import style from "../styles/Sidebar.module.scss";
+
 import logo from "../assets/logo.png";
 import home_icon from "../assets/icon-Home.svg";
 import magazine_icon from "../assets/icon-Magazine.svg";
 import cinema_icon from "../assets/icon-cinema.svg";
 import wishlist_icon from "../assets/icon-wishlist.svg";
+
+const NavItemsData = [
+  {
+    src: home_icon,
+    alt: "home_icon",
+    nav_name: "홈",
+  },
+  {
+    src: magazine_icon,
+    alt: "magazine_icon",
+    nav_name: "매거진",
+  },
+  {
+    src: cinema_icon,
+    alt: "cinema_icon",
+    nav_name: "상영회",
+  },
+  {
+    src: wishlist_icon,
+    alt: "home_icon",
+    nav_name: "위시리스트",
+  },
+];
 
 const Sidebar = () => {
   return (
@@ -11,40 +37,18 @@ const Sidebar = () => {
       <div className={style.sidebar}>
         <div className={style.logo}>
           <a href="{()=>false}">
-            <img src={logo} alt="mudi_logo"/>
+            <img src={logo} alt="mudi_logo" />
           </a>
         </div>
         <ul className={style.nav}>
-          <li>
-            <a href="{()=>false}">
-              <img src={home_icon} alt="home_icon"/>
-              <span>홈</span>
-            </a>
-          </li>
-          <li>
-            <a href="{()=>false}">
-              <img src={magazine_icon} alt="magazine_icon"/>
-              <span>매거진</span>
-            </a>
-          </li>
-          <li>
-            <a href="{()=>false}">
-              <img src={cinema_icon} alt="cinema_icon"/>
-              <span>상영회</span>
-            </a>
-          </li>
-          <li>
-            <a href="{()=>false}">
-              <img src={wishlist_icon} alt="wishlist_icon"/>
-              <span>위시리스트</span>
-            </a>
-          </li>
+          {NavItemsData.map((data, index) => {
+            return <NavItem key={index} data={data} />;
+          })}
         </ul>
-        <div className={style.profile}> 
-        </div>
+        <div className={style.profile}></div>
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
