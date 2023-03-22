b4d356a88d1f35d7f60c88e01ccb864f7f1bba50
sujeong126 <hsjhsj8010@naver.com>
2023-03-22 17:52:03 +0900

    Design: Sidebar UI

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,47 @@
 import style from "../styles/Sidebar.module.scss";
+import logo from "../assets/logo.png";
+import home from "../assets/home.png";
+import book from "../assets/book.png";
+import movie from "../assets/movie.png";
+import heart from "../assets/heart.png";
+
 function Sidebar() {
   return (
     <div className={style.container}>
       <div className={style.sidebar}>
-        <a href="{()=>false}" className={style.logo}>
-          해결
-        </a>
-        <ul>
+        <div className={style.logo}>
+          <a href="{()=>false}">
+            <img src={logo} alt="mudi_logo"/>
+          </a>
+        </div>
+        <ul className={style.nav}>
           <li>
             <a href="{()=>false}">
-              <i>icon</i>
-              <p>홈</p>
+              <img src={home} alt="home_img"/>
+              <span>홈</span>
+            </a>
+          </li>
+          <li>
+            <a href="{()=>false}">
+              <img src={book} alt="book_img"/>
+              <span>매거진</span>
+            </a>
+          </li>
+          <li>
+            <a href="{()=>false}">
+              <img src={movie} alt="movie_img"/>
+              <span>상영회</span>
+            </a>
+          </li>
+          <li>
+            <a href="{()=>false}">
+              <img src={heart} alt="heart_img"/>
+              <span>위시리스트</span>
             </a>
           </li>
         </ul>
+        <div className={style.profile}> 
+        </div>
       </div>
     </div>
   );
