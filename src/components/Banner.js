33b29ab65bb6d5dd5c063c9ece58a11687a1cf9d
sujeong126 <hsjhsj8010@naver.com>
2023-03-30 14:03:50 +0900

    Style: Add banner image, UI size fix

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,9 @@
 import style from "../styles/Banner.module.scss";
- // 테스트 주석입니다
+
+import banner01 from "../assets/banner01.png";
+import banner02 from "../assets/banner02.png";
+import banner03 from "../assets/banner03.png";
+
 const Banner = () => {
   return (
     <div className={style.banner}>
@@ -7,12 +11,15 @@ const Banner = () => {
         <div className={style.slider}>
           <ul className={style.slide_show}>
             <li id="slide01" className={style.slide}>
+              <img src={banner01} alt="banner_image_01" />
               <div className={style.snapper}></div>
             </li>
             <li id="slide02" className={style.slide}>
+              <img src={banner02} alt="banner_image_02" />
               <div className={style.snapper}></div>
             </li>
             <li id="slide03" className={style.slide}>
+              <img src={banner03} alt="banner_image_03" />
               <div className={style.snapper}></div>
             </li>
           </ul> 
