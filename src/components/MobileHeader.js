7f795009c791d0c0f8d74f85fd346a468c114996
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-01 01:24:19 +0900

    Feat: Add header only for mobile size

---
+++ src/components/MobileHeader.js
@@ -0,0 +1,7 @@
+const MobileHeader = () => {
+  return (
+  <div>모바일 헤더입니다.</div>
+  )
+}
+
+export default MobileHeader;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,29 @@
+import { useState, useEffect } from "react";
+
 import Aspoiler from "../components/Aspoiler";
 import Banner from "../components/Banner";
 import FullMovie from "../components/FullMovie";
+import Footer from "../components/Footer"
+import MobileHeader from "../components/MobileHeader"
 
 const Home = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowWidth = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowWidth);
+    return () => {
+      window.removeEventListener("resize", handleWindowWidth);
+    };
+  });
+
   return(
     <div>
+      {windowWidth < 391 && <MobileHeader />}
      <Banner />
      <Aspoiler />
      <FullMovie />
+     <Footer />
     </div>
   )
 }
